5c680d577ac70f1e89a118800c005ea19201e7a3
Jessialbero <jessialbero@gmail.com>
2022-08-03 22:25:06 -0300

    Agregando los elementos de la App

---
+++ src/components/Personajes.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Personajes() {
+    return (
+        <article>
+        <img src="img/Cars.jpg" alt="Cafetera" class="product_img"/>
+        <div class="main-article">
+            <p class="price">$6.770</p>
+            <p class="discount">40% off</p>
+            <h4 class="description">Cafetera Moulinex</h4>
+        </div>
+        </article>
+    );
+}
+
+  export default Personajes;
